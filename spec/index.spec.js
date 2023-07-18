ae5bd4737294fd7cdc7f4885b8cefb01064c095b
Madeline <moon.madeline@gmail.com>
2023-07-18 10:47:52 -0500

    Implement pre/append, size, head/tail

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,11 +1,38 @@
-const LinkedList = require("../src/index.js");
+import linkedList from '../src/index.js'
+import Node from '../src/index.js'
+
 
 describe("Linked list", function(){
 
     it("should return an empty object when first called", function(){
-        const myList = new LinkedList();
-        console.log(myList);
-        expect(typeof myList).toEqual('Object');
+        const myList = linkedList();
+        expect(typeof myList.getList()).toEqual('object');
+    });
+
+    it("should add a single value to the list using append", function(){
+        const myList = linkedList();
+        myList.append("My value");
+        expect(myList.head().value).toEqual("My value");
+        expect(myList.head().nextNode).toEqual(null);
+    });
+
+    it("should append two values", function(){
+        const myList = linkedList();
+        myList.append("My value");
+        myList.append("My second value");
+        expect(myList.size()).toEqual(2);
+        expect(myList.tail().value).toEqual("My second value");
+        expect(myList.head().value).toEqual("My value");
+    });
+
+    it("should add a value to the beginning of the list using prepend", function(){
+        const myList = linkedList();
+        myList.append("My value");
+        myList.append("My second value");
+        myList.prepend("My third value");
+        expect(myList.size()).toEqual(3);
+        expect(myList.head().value).toEqual("My third value");
+        expect(myList.tail().value).toEqual("My second value");
     });
 
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,72 @@
-class LinkedList {
-    constructor() {
+const linkedList = () => {
+    
+    const prepend = (value) => {
+        let newNode = new Node(value);
+        if (size() <= 0) {
+            list['head'] = newNode;
+        } else {
+            let firstNode = head();
+            newNode.nextNode = firstNode;
+            list[`${size()}`] = firstNode;
+            list['head'] = newNode;
+        }
     }
+
+    const size = () => {
+        return Object.keys(list).length
+    }
+
+    const head = () => {
+        return list['head']
+    }
+
+    const tail = () => {
+        return list['tail']
+    }
+
+    const append = (value) => {
+        if (size() <= 0){
+            prepend(value)
+        } else {
+            let newNode = new Node(value);
+            if (size() === 1) {
+                let lastNode = head();
+                lastNode.nextNode = newNode;
+            } else {
+                let lastNode = tail();
+                lastNode.nextNode = newNode;
+                list[`${size()}`] = lastNode;
+            }
+            list['tail'] = newNode;
+        }
+    }
+
+    const at = (index) => {
+        // go from one to next until 
+
+    }
+
+    const pop = () => {
+
+    }
+
+    const contains = () => {
+
+    }
+
+    const find = () => {
+
+    }
+
+    const toString = () => {
+
+    }
+
+    const list = {};
+
+    const getList = () => list;
+
+    return { append, prepend, size, head, tail, getList, list }
 }
 
 class Node {
@@ -10,4 +76,8 @@ class Node {
     }
 }
 
-module.exports = {LinkedList, Node}
+const myList = linkedList();
+
+
+export default linkedList;
+export { Node } 
